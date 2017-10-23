232a639de3fee8e8d69f52d222d91837333e2b6c
Kaela Schaefer <kaelaxxanne@gmail.com>
2017-10-23 07:41:14 -0700

    Update to include final tests

--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -15,16 +15,13 @@ log = logging.getLogger(__name__)
 Helper Function to Allow for 1 minute difference
 to account for rounding errors
 """
-def nearly(x, y):
-    if (x < y):
-        time_one = y
-        time_two = x
+def nearly(time_one, time_two):
+    if time_one < time_two:
+        diff = arrow.get(time_two) - arrow.get(time_one)
     else:
-        time_one = x
-        time_two = y
-    diff = arrow.get(time_one) - arrow.get(time_two)
+        diff = arrow.get(time_one) - arrow.get(time_two)
     minutes = diff.seconds/60
-    return not ( int(minutes) > 1)
+    return int(minutes) <= 1
 
 """
 Testing Opening Times
@@ -33,8 +30,8 @@ Testing Opening Times
 def test_short_control():
     """
     Test a control set to exactly 15km
-    Should return exactly one hour later
     """
+    assert nearly(open_time(15, 200, "2018-01-01T00:00:00+00:00"), "2018-01-01T00:26:00+00:00")
     assert nearly(close_time(15, 200, "2017-10-18T06:00:00-08:00"), "2017-10-18T07:00:00-08:00")
 
 def test_edge_case():
@@ -50,17 +47,27 @@ def test_edge_case():
     assert nearly(close_time(600, 1000, "2018-01-01T00:00:00+00:00"), "2018-01-02T16:00:00+00:00")
     assert nearly(close_time(610, 1000, "2018-01-01T00:00:00+00:00"), "2018-01-02T16:53:00+00:00")
 
-def test_bad_input_string():
+def test_late_start_time():
     """
-    Test that the algorithms can properly handle bad time strings
+    Test a control on a late start time 
+    to show days roll over correctly
     """
-    assert not close_time(590, 1000, "") == "2018-01-02T15:20:00+00:00"
-    assert not close_time(200, 200, "abcdefg") == "2018-01-02T16:53:00+00:00"
-    assert not open_time(590, 1000, "") == "2018-01-02T15:20:00+00:00"
-    assert not open_time(200, 200, "abcdefg") == "2018-01-02T16:53:00+00:00"
+    assert nearly(open_time(100, 200, "2018-01-01T23:00:00+00:00"), "2018-01-02T01:56:00+00:00")
+    assert nearly(close_time(100, 200, "2018-01-01T23:00:00+00:00"), "2018-01-02T05:40:00+00:00")
 
-def test_bad_input_nums():
+def test_ending_control():
     """
-    Test that the algorithms can handle bad nums
+    A control that is larger than the total brevet distance but smaller than
+    total distance + 10% should return the same open/close times as the total distance
     """
-    assert not close_time(-5, 150, "2018-01-02T16:53:00+00:00") == "2018-01-02T16:53:00+00:00"
+    assert nearly(close_time(200, 200, "2018-01-01T23:00:00+00:00"), close_time(210, 200, "2018-01-01T23:00:00+00:00"))
+    assert nearly(open_time(200, 200, "2018-01-01T23:00:00+00:00"), open_time(210, 200, "2018-01-01T23:00:00+00:00"))
+
+def test_very_short_control_oddity():
+    """
+    Testing example 3 from the algorithm description
+    An 890km control on a 1000km brevet
+    """
+    assert nearly(close_time(10, 200, "2018-01-01T00:00:00+00:00"), "2018-01-01T00:40:00+00:00")
+    
+    
